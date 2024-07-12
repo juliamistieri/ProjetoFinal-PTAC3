44ec91ac8463815635796f6ea1c8b7b01e56a51f
Alexig2 <alex4342on@gmail.com>
2024-07-12 20:46:37 -0300

    Implementando a página Home - Requisito #1

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,13 @@
 const nextConfig = {
 
     images:{
-        domains:["m.media-amazon.com"]
+        domains:[
+            "m.media-amazon.com",
+            'cinemacomrapadura.com.br',
+            'static.wikia.nocookie.net',
+            'ingresso-a.akamaihd.net',
+            'br.web.img3.acsta.net',
+        ]
     }
 };
 
--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,45 @@
+'use client'
 import Image from "next/image";
 import "./styles.css";
 import Link from "next/link";
+import { listaDeFilmes } from "./api/route";
+import { useState, useEffect } from "react"
+import styles from "./styles/main.module.css";
 
 export default function Home() {
 
+  const [listaFilmes, setListaFilmes] = useState([]);
+  const [listaCompleta, setListaCompleta] = useState([]);
+
+  useEffect(() => {
+    const getFilmes = () => {
+      setListaFilmes(listaDeFilmes);
+      setListaCompleta(listaDeFilmes);
+    }
+    getFilmes();
+  }, []);
+
   return (
-    <main>
-     
+    <main className={styles.main}>
+      {listaFilmes.map((filme) => (
+        <div className={styles.card} key={filme.id}>
+          {/* <div className={styles.titulo}>{filme.titulo.slice(0, 40)}...</div> */}
+
+          <Image className={styles.img} width={300} height={400} src={filme.linkImg} />
+
+          {/* <div className={styles.propriedades}>{filme.categoria}</div>
+
+          <div className={styles.propriedades}>{filme.anoLancamento}</div>
+
+          <div className={styles.propriedades}>{filme.duracao}</div>
+
+          <div className={styles.ultimaProp}>{filme.generos.slice(0, 40)}...</div> */}
+
+          <Link href={'/api/' + filme.id}>
+            <button>Ver Mais</button>
+          </Link>
+        </div>
+      ))}
     </main>
   );
 }
