1bfaab3745cce66491542cab8e65ade0f2dac5b2
Alexig2 <alex4342on@gmail.com>
2024-07-17 12:19:24 -0300

    UseState e useEffect - Requisito #3
    Como cada requisito deve ter um commit estou colocando este, mas ele já foi implementado no requisito #1.

--- src/app/page.js
+++ src/app/page.js
@@ -12,7 +12,7 @@ export default function Home() {
   const [listaCompleta, setListaCompleta] = useState([]);
 
   useEffect(() => {
-    const getFilmes = () => {
+    const getFilmes = () => { 
       setListaFilmes(listaDeFilmes);
       setListaCompleta(listaDeFilmes);
     }
