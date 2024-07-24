a8a1f5f8084809d22de3d8f444f6fda28ff90d0c
juliamistieri <julia.mistieri02@gmail.com>
2024-07-24 10:28:15 -0400

    commit

--- src/app/filmes/[id]/page.js
+++ src/app/filmes/[id]/page.js
@@ -8,30 +8,30 @@ export default async function Filme({params}) {
     const data = await response.json();
     
     return (
-      <main className={styles.main}>
-  <div className={styles.perfil}>
-    <div className={styles.heroSection}>
-      <Image
-        src={data.linkImg}
-        layout="fill"
-        objectFit="cover"
-        className={styles.perfil}
-      />
-      <div className={styles.heroOverlay}></div>
-      <div className={styles.heroContent}>
-        <h1 className={styles.heroTitle}>{data.titulo}</h1>
-        <p className={styles.heroSynopsis}>{data.categoria}</p>
-        <p className={styles.heroSynopsis}>{data.anoLancamento}</p>
-        <p className={styles.heroSynopsis}>{data.duracao}</p>
-        <p className={styles.heroSynopsis}>{data.generos}</p>
-        <div className={styles.heroButtons}>
-        <Link className={styles.heroButton} href="/">
-              Assistir
-            </Link>
-        </div>
-      </div>
-    </div>
-  </div>
-</main>
+        <main className={styles.main}>
+            <div className={styles.perfil}>
+                <div className={styles.section}>
+                    <Image
+                        src={data.linkImg}
+                        layout="fill"
+                        objectFit="cover"
+                        className={styles.perfil}
+                    />
+                    <div className={styles.overlay}></div>
+                    <div className={styles.content}>
+                        <h1 className={styles.title}>{data.titulo}</h1>
+                        <p className={styles.synopsis}>{data.categoria}</p>
+                        <p className={styles.synopsis}>{data.anoLancamento}</p>
+                        <p className={styles.synopsis}>{data.duracao}</p>
+                        <p className={styles.synopsis}>{data.generos}</p>
+                        <div className={styles.buttons}>
+                            <Link className={styles.button} href="/">
+                                Assistir
+                            </Link>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </main>
     );
-  }
+}
